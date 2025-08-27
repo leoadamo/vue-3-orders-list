856abf6464b8c94ee9200ac3000dc43f230fc4c0
Leonardo Adamoli <leo.adamoli@gmail.com>
2025-08-27 20:09:14 -0300

    fix(vite): broken vitest config
    this will fix an issue with the vitest config which whas breaking the build proccess

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
+// DEPENDENCIES
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+// TYPES
+import type { UserConfig } from "vite";
+
+// DEPENDENCIES
+import { fileURLToPath } from "node:url";
+import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
-  viteConfig,
+  viteConfig as UserConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/**"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
     },
   }),
-)
+);
